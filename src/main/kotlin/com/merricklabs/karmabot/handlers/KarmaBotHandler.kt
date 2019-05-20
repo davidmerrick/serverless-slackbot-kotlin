747959f810292d8ed593278db7ebdf463e767759
David Merrick <david@david-merrick.com>
2019-05-20 14:43:20 -0700

    Endpoint is 502'ing

--- src/main/kotlin/com/merricklabs/karmabot/handlers/KarmaBotHandler.kt
+++ src/main/kotlin/com/merricklabs/karmabot/handlers/KarmaBotHandler.kt
@@ -1,4 +1,4 @@
-package com.merricklabs.karmabot
+package com.merricklabs.karmabot.handlers
 
 import com.amazonaws.services.lambda.runtime.Context
 import com.amazonaws.services.lambda.runtime.RequestHandler
